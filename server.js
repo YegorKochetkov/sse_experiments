fe04b7342af76945792ba43f5fbd6184601919ea
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-01-30 16:36:49 +0200

    feat: create SSE example

---
+++ server.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import http from "http";
+
+let dataForSSE;
+
+http
+	.createServer((req, res) => {
+		const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+
+		if (reqUrl.pathname === "/send-message") {
+			const message = reqUrl.searchParams.get("message");
+			dataForSSE = message;
+			res.end();
+			return;
+		}
+
+		if (req.url === "/get-stream") {
+			sse(req, res);
+			console.log("sse started");
+			return;
+		}
+
+		fs.createReadStream("index.html").pipe(res);
+	})
+	.listen(8080, () => {
+		console.log("Server running at http://127.0.0.1:8080");
+	});
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function sse(_req, res, message) {
+	res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader("Cache-Control", "no-cache");
+	res.setHeader("Connection", "keep-alive");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	let id = 0;
+
+	const intervalId = setInterval(() => {
+		const message = dataForSSE ?? `Some random data: ${getRandomInt(100)}`;
+		res.write(`data: ${message}\n`);
+		res.write(`id: ${++id}\n\n`);
+	}, 1000);
+
+	setTimeout(() => {
+		clearInterval(intervalId);
+		res.write("event: sse-stopped\n");
+		res.write("data: sse stopped\n");
+		res.write(`id: ${++id}\n\n`);
+		console.log("sse stopped");
+		res.end("Ok");
+	}, 60_000);
+}
+
+/**
+ * @param {number} max
+ */
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
